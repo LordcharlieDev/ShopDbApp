ad5756a88edf5d6f4cd8a3176ce8b0468c0cdd10
Oleksandr <sasha2003demchuk@gmail.com>
2022-02-20 17:45:17 +0200

    Add class Cities.

---
+++ ShopDbApp/Models/Cities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDbApp
+{
+    public class Cities
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int CounrtyId { get; set; }
+    }
+}
